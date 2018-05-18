08a853b700a996abc7045c1182d6fcc51be9697d
field <field@ccom.unh.edu>
2018-05-18 17:19:45 -0400

    initial commit

---
+++ src/kongsberg_em_control/em.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import socket
+
+# udp port 4001: R20
+# udp port 4002: R00, R10, R12
+#
+class EM:
+    def __init__(self, sis_ip, model_number='2040'):
+        self.model_number = model_number
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.destination = (sis_ip,4002)
+
+    def stopPinging(self):
+        #talker id, picking BS for backseat for now...
+        msg = '$BSR00,EMX='+self.model_number+',ROP=,SID=,PLN=,PLL=,COM=\r\n'
+        self.socket.sendto(msg,self.destination)
+
+    def startPinging(self):
+        msg = '$BSR10,EMX='+self.model_number+',ROP=,SID=,PLN=,PLL=,COM=\r\n'
+        self.socket.sendto(msg,self.destination)
+
+    def startLine(self,line):
+        msg = '$BSR12,EMX='+self.model_number+',ROP=,SID=,PLN=,PLL='+line+',COM=\r\n'
+        self.socket.sendto(msg,self.destination)
+
+if __name__ == '__main__':
+    em = EM('stormi')
+    em.startLine('11')
