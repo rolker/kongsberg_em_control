3d69e3a4c4e3ac5308b82ae769a02cc26ddb070f
Roland Arsenault <roland@ccom.unh.edu>
2018-06-01 14:20:27 -0400

    Added ROS service to control sonar.

---
+++ nodes/kongsberg_em_control_node.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from kongsberg_em_control.srv import *
+import rospy
+import kongsberg_em_control.em
+
+sonar = None
+
+def handle_em_command(req):
+    if req.requested_mode == EMControlRequest.SONAR_REQUEST_STOP_PINGING:
+        sonar.stopPinging()
+        return EMControlResponse("ok")
+    if req.requested_mode == EMControlRequest.SONAR_REQUEST_START_PINGING:
+        sonar.startPinging()
+        return EMControlResponse("ok")
+    if req.requested_mode == EMControlRequest.SONAR_REQUEST_START_LINE:
+        sonar.startLine(str(req.line_number))
+        return EMControlResponse("ok")
+    return EMControlResponse("unknown mode")
+
+
+def EMControlServer():
+    global sonar
+    while not rospy.has_param('sonar'):
+        print 'waiting for sonar parameters...'
+        rospy.sleep(1)
+    
+    sonar_params = rospy.get_param('sonar')
+    sonar = kongsberg_em_control.em.EM(sonar_params['host'],str(sonar_params['model']))
+
+    rospy.init_node('EMControl')
+    
+    s = rospy.Service('/sonar/control',EMControl, handle_em_command)
+    s.spin()
+    
+    
+EMControlServer()
